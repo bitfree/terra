393cd897fa1ae995edaef9f98181fea5ec0e9bd1
gichoi <bitfree@gmail.com>
2020-08-17 06:47:52 +0900

    comment out about apt and apache2

--- seoul/main.tf
+++ seoul/main.tf
@@ -7,12 +7,22 @@ resource "aws_instance" "example" {
   instance_type = "t2.micro"
 
   vpc_security_group_ids = ["${aws_security_group.instance.id}"]
-  
+ 
+
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello Terraform.." > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p 80 &
               EOF
+
+/*
+user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update 
+              sudo apt-get install -y  apache2
+              sudo service apache2 start 
+              EOF
+*/
   tags = {
     Name:"terraform-example"
   }
@@ -25,15 +35,15 @@ resource "aws_security_group" "instance" {
   }
 
   ingress {
-    from_port = 8080
-    to_port = 8080
+    from_port = 80
+    to_port = 80
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
  
   egress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
