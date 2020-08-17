0ca354c202c31334ff1be004cab51ae869049ebe
gichoi <bitfree@gmail.com>
2020-08-18 07:05:52 +0900

    makes instance_ex1/main.tf

---
+++ seoul/instance_ex1/main.tf
@@ -0,0 +1,65 @@
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+resource "aws_instance" "example" {
+  ami = "ami-05438a9ce08100b25"
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+ 
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello Terraform.." > index.html
+              nohup busybox httpd -f -p 80 &
+              EOF
+
+/*
+user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update 
+              sudo apt-get install -y  apache2
+              sudo service apache2 start 
+              EOF
+*/
+  tags = {
+    Name:"terraform-example"
+  }
+ 
+}
+
+resource "aws_security_group" "instance" {
+  tags = {
+    Name:"terraform-example-instance"
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
+  
+  ingress {
+    from_port = 22 
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+  egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
